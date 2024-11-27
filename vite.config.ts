1291eb9be2d1d02035b84e0f9f3b7c286b9057de
kinetic4 <pratyushbhargava6@gmail.com>
2024-11-27 16:23:21 +0530

    vite-config change

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 5173, // Ensure the port is correctly set
+  },
   plugins: [react()],
 })
