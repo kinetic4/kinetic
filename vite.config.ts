06531392b49f849a239ab033a16c284613208c05
kinetic4 <pratyushbhargava6@gmail.com>
2024-11-28 01:04:09 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  server: {
-    open: false,
-    port: (process.env.PORT ? parseInt(process.env.PORT) : 3000),
-    host: '0.0.0.0'
-  },
-  preview: {
-    port: (process.env.PORT ? parseInt(process.env.PORT) : 3000),
-    host: '0.0.0.0'
-  },
   plugins: [react()],
-  build: {
-    target: 'esnext',
+  preview: {
+    port: (process.env.PORT ? parseInt(process.env.PORT) : 4173),
+    host: '0.0.0.0'
   },
-  optimizeDeps: {
-    exclude: ['gsap']
-  }
 })
